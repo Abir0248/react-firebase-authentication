196752489e78aa6682596dbf4e453e7522c33482
Abir0248 <abir15-6789@diu.edu.bd>
2022-04-07 03:01:43 +0600

    google popup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
-    console.log("working");
+    // console.log("working");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <button onClick={handleGoogleSignIn} className="g-btn">
+        Google Sign In
+      </button>
     </div>
   );
 }
